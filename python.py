dcf01672d0235769a6f073e08c4b275d47275ca1
hoanghuyrua240 <hoanghuyrua240@gmail.com>
2025-03-23 09:25:33 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,1 @@
-I’d rather be coding.Git happens.Real programmers count from 0.If it ain’t broke, improve it until it is.python.py
+Exception in thread "main" java.lang.NullPointerException.I’d rather be coding.Git happens.Real programmers count from 0.If it ain’t broke, improve it until it is.python.py
