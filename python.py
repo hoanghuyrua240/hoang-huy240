c1b929b7b4b0eaa9936cfa283e425296f1ea10f3
hoanghuyrua240 <hoanghuyrua240@gmail.com>
2025-03-24 09:25:33 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,1 @@
-Exception in thread "main" java.lang.NullPointerException.I’d rather be coding.Git happens.Real programmers count from 0.If it ain’t broke, improve it until it is.python.py
+Real developers don’t need Stack Overflow… said no developer ever.Exception in thread "main" java.lang.NullPointerException.I’d rather be coding.Git happens.Real programmers count from 0.If it ain’t broke, improve it until it is.python.py
